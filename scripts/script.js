046933cd735a1db9cdf7e3238d40ddd63b0327ee
Koayz <58449532+Koayz@users.noreply.github.com>
2022-04-26 12:43:30 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,19 @@
 // JavaScript Document
 
 // Dit is de eevee
-var eevee = document.querySelector(".eevee");
+var eevee = document.querySelector("article:nth-of-type(2) img");
+var eeveeNaam = document.querySelector("article:nth-of-type(2) h4");
 
 // Klik op deze variable om het te veranderen
 var duskstone = document.querySelector(".duskstone");
+
+var eeveeName = document.querySelector("h4")
 
 duskstone.addEventListener("click", verander);
 
 function verander() {
   eevee.src = "../images/Eevees/Flareon.png";
+  eeveeNaam.innerHTML = "Flareon";
 }
+
+
